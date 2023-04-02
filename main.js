16c3d23e25704ecdfed6ec9a9f008c8d224e8c28
Vikash-shandilya <vikashjha041@gmail.com>
2023-04-02 12:27:15 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// console.log('hello dom')
+// console.log(document);
+// console.log(document.URL);
+
+// document.title=123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.title);
+// console.log(document.forms);
+
+
+var ll=document.getElementById("header-title");
+var header=document.getElementById('main-header')
+ll.style.borderBottom='solid 2px #000'
+
+var bold=document.getElementsByClassName('title')
+bold[0].style.fontWeight='bold';
+bold[0].style.color='green'
