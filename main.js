291302c3f0894d544a99f852802b5eeeee44bc4f
Vikash-shandilya <vikashjha041@gmail.com>
2023-04-02 13:00:30 +0530

    second commit

--- main.js
+++ main.js
@@ -9,10 +9,20 @@
 // console.log(document.forms);
 
 
-var ll=document.getElementById("header-title");
-var header=document.getElementById('main-header')
-ll.style.borderBottom='solid 2px #000'
+// var ll=document.getElementById("header-title");
+// var header=document.getElementById('main-header')
+// ll.style.borderBottom='solid 2px #000'
 
-var bold=document.getElementsByClassName('title')
-bold[0].style.fontWeight='bold';
-bold[0].style.color='green'
+// var bold=document.getElementsByClassName('title')
+// bold[0].style.fontWeight='bold';
+// bold[0].style.color='green'
+
+let gbc=document.getElementsByClassName('list-group-item')
+
+gbc[2].style.backgroundColor='green'
+
+gbc[0].style.fontWeight='bold'
+gbc[1].style.fontWeight='bold'
+gbc[2].style.fontWeight='bold'
+gbc[3].style.fontWeight='bold'
+
