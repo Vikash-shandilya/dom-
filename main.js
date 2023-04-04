15b94e3ec93d7bb513ade4c36f3af7b825072d15
Vikash-shandilya <vikashjha041@gmail.com>
2023-04-04 20:14:25 +0530

    task 8

--- main.js
+++ main.js
@@ -64,76 +64,112 @@
     
 // }
 
-parentelement
-let itemselect=document.querySelector('#items')
-console.log(itemselect);
-itemselect.parentNode.style.backgroundColor='green'
-//last element child
+// parentelement
+// let itemselect=document.querySelector('#items')
+// console.log(itemselect);
+// itemselect.parentNode.style.backgroundColor='green'
+// //last element child
 
-console.log(itemselect.lastElementChild)
-itemselect.lastElementChild.textContent='newitem4'
-//lastchild
+// console.log(itemselect.lastElementChild)
+// itemselect.lastElementChild.textContent='newitem4'
+// //lastchild
 
-console.log(itemselect.lastChild);
-// this is not a good practise to use last child we should 
-//use children instead
-//create child
-// there is nothing callled create child its create elememnt
+// console.log(itemselect.lastChild);
+// // this is not a good practise to use last child we should 
+// //use children instead
+// //create child
+// // there is nothing callled create child its create elememnt
 
-//firstchild this is also not a very good practise
-console.log(itemselect.firstChild);
+// //firstchild this is also not a very good practise
+// console.log(itemselect.firstChild);
 
-//nextsibling and next element sibling
+// //nextsibling and next element sibling
 
-let sibling=document.querySelectorAll('.list-group-item')
-console.log(sibling[0]);
-console.log(sibling[0].nextSibling);
+// let sibling=document.querySelectorAll('.list-group-item')
+// console.log(sibling[0]);
+// console.log(sibling[0].nextSibling);
 
-console.log(sibling[0].nextElementSibling.textContent);
+// console.log(sibling[0].nextElementSibling.textContent);
 
-//previous sibling and previous element sibling
+// //previous sibling and previous element sibling
 
-console.log(sibling[4].previousSibling.textContent);
-console.log(sibling[3].previousElementSibling.textContent);
+// console.log(sibling[4].previousSibling.textContent);
+// console.log(sibling[3].previousElementSibling.textContent);
 
-//createelement
+// //createelement
 
-let newdiv=document.createElement('div')
+// let newdiv=document.createElement('div')
 
-//add class
-newdiv.className='hii'
-//add id
-newdiv.id='hello'
-newdiv.setAttribute('title','checking')
+// //add class
+// newdiv.className='hii'
+// //add id
+// newdiv.id='hello'
+// newdiv.setAttribute('title','checking')
 
-//create textnode
-let newdivtext=document.createTextNode('hello world')
+// //create textnode
+// let newdivtext=document.createTextNode('hello world')
 
-//appendchild
+// //appendchild
 
-newdiv.appendChild(newdivtext)
+// newdiv.appendChild(newdivtext)
 
-//append that newdiv before h1
+// //append that newdiv before h1
 
-let container=document.querySelector('header .container')
-let h1=document.querySelector('header h1')
+// let container=document.querySelector('header .container')
+// let h1=document.querySelector('header h1')
 
-container.insertBefore(newdiv,h1)
-newdiv.style.color='orange'
+// container.insertBefore(newdiv,h1)
+// newdiv.style.color='orange'
 
-//add hello world before item1
+// //add hello world before item1
 
-let newdiv1=document.createElement('div')
+// let newdiv1=document.createElement('div')
 
-let h2=document.createTextNode('hello world')
-newdiv1.className='hello world'
+// let h2=document.createTextNode('hello world')
+// newdiv1.className='hello world'
 
-newdiv1.append(h2)
-let container1=document.querySelector('#items')
-let appendelement=document.querySelector('.item-group-item')
-let newlistgroupitem=container1.children[0]
-console.log(newlistgroupitem);
-container1.insertBefore(newdiv1,newlistgroupitem)
+// newdiv1.append(h2)
+// let container1=document.querySelector('#items')
+// let appendelement=document.querySelector('.item-group-item')
+// let newlistgroupitem=container1.children[0]
+// console.log(newlistgroupitem);
+// container1.insertBefore(newdiv1,newlistgroupitem)
+
+//on clicking delete button we should remove the new li tag
+
+let deletebtn=document.getElementsByClassName("btn btn-danger btn-sm float-right delete")
+console.log(deletebtn);
+
+for (let i= 0; i<deletebtn.length; i++) {
+    deletebtn[i].addEventListener('click',deleteitem)
+    
+    
+}
+function deleteitem(e)
+{
+    if(confirm(' are you sure'))
+    {
+        let itemtoremove=e.target.parentElement;
+        let parentelement=e.target.parentElement.parentElement;
+        parentelement.removeChild(itemtoremove)
+        
+
+    }
+    
+}
+
+//create the edit button next to delete button 
+let listgroup = document.getElementsByClassName("list-group-item");
+for (let i = 0; i < listgroup.length; i++) {
+    let editbutton = document.createElement('button');
+    editbutton.setAttribute('class', 'btn btn-orange btn-sm float-right edit');
+    editbutton.textContent = 'Edit';
+
+    listgroup[i].appendChild(editbutton);
+    
+    
+}
+
 
 
 
